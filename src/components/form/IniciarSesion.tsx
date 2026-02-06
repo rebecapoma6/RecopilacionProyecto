d5034041179567ffde00db1dee62e84b0cd979cc
ronicleal <ralealp01@iesalbarregas.es>
2026-02-06 18:50:12 +0100

    ✅Iniciar Sesion

---
+++ src/components/form/IniciarSesion.tsx
@@ -0,0 +1,18 @@
+
+interface LoginDataProps {
+    email: string;
+    password: string;
+}
+
+interface LoginErrorProps{
+    email?: string;
+    password?: string;
+}
+
+export default function LoginForm(){
+    // 1. Estado para los datos
+    // const[formData, setFormData] = useState<LoginDataProps>({
+    //     email: "",
+    //     password: "",        
+    // });
+}
