1b17e52f71bb081f86f09a026c0437780d09e94e
Rebeca Poma <mrebecapoma.6@gmail.com>
2026-02-10 11:23:30 +0100

    🔥

--- src/pages/PageRecuperarPass.tsx
+++ src/pages/PageRecuperarPass.tsx
@@ -4,7 +4,6 @@ export default function PageRecuperarPass(){
     return (
     <main className="min-h-screen flex items-center justify-center bg-gray-50">
       <div className="w-full max-w-md p-6">
-        {/* Aquí llamas a tu componente */}
         <RecuperarPass />
       </div>
     </main>
