d04796fdb95c4258ee3a834462bf3a319ac669e9
Rebeca Poma <mrebecapoma.6@gmail.com>
2026-03-08 16:38:38 +0100

    🔥

--- src/App.tsx
+++ src/App.tsx
@@ -1,50 +1,4 @@
-// import { Routes, Route } from 'react-router-dom';
-// import Navbar from './components/navbar/Navbar';
-// import Footer from './components/footer/Footer';
-// // import Products from './pages/';
-// import Registro from './components/form/Registro';
-// // import ProductosSupabase from './pages/ProductosSupabase';
-// //import RecuperarPass from './pages/RecuperarPass';
-// import AgregarItems from './pages/AgregarItemsPage';
-// import './App.css';
-// import PageRecuperarPass from './pages/RecuperarPassPage';
-// import IniciarSesionPage from './pages/IniciarSesionPage';
-// import HomePage from './pages/HomePage';
-// import AgregarItemsPage from './pages/AgregarItemsPage';
-// import PageRecuperarPassPage from './pages/RecuperarPassPage';
-// import RegistroPage from './pages/RegistroPage';
-// import ProductosSupabase from './pages/ProductosSupabase';
-
-// function App() {
-//   return (
-//     <div className="app-container flex flex-col min-h-screen">
-//       <Navbar />
-//       <main className="grow">
-//         <Routes>
-//           <Route path="/" element={<HomePage />} />
-//           {/*<Route path="/profile" element={<PerfilUsuarioPage />} />*/}
-//           <Route path="/products" element={<ProductosSupabase />} />
-//           <Route path="/iniciarSesion" element={<IniciarSesionPage />} />
-//           <Route path="/registro" element={<RegistroPage />} />
-//           {/* <Route path="/productos-supabase" element={<ProductosSupabase />} /> */}
-//           {/* <Route path="/recuperar-pass" element={<RecuperarPass />} /> */}
-//           <Route path="/agregar-items" element={<AgregarItemsPage />} />
-//           <Route path="/recuperarPass" element={<PageRecuperarPassPage/>} />
-  
-//         </Routes>
-//       </main>
-//       <div className="footer-container">
-//         <Footer />
-//       </div>
-//     </div>
-//   );
-// }
-
-// export default App;
-
-
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
-
 import LandingLayout from './layouts/LandingLayout';
 import AuthLayout from './layouts/AuthLayout';
 import AppLayout from './layouts/AppLayout';
@@ -54,13 +8,9 @@ import HomePage from './pages/HomePage';
 import IniciarSesionPage from './pages/IniciarSesionPage';
 import RegistroPage from './pages/RegistroPage';
 import ProductosSupabase from './pages/ProductosSupabase';
-//import AgregarItemsPage from "./pages/AgregarItemsPage";
 import PageRecuperarPassPage from './pages/RecuperarPassPage';
 import ModificarDatosPage from "./pages/ModificarDatosPage";
-
-
 import PerfilUsuarioPage from './pages/PerfilUsuarioPage'; 
-
 import ProtectedRoute from "./router/ProtectedRoute";
 import PublicRoute from "./router/PublicRoute";
 import AdminPage from "./pages/AdminPage";
@@ -98,6 +48,7 @@ const router = createBrowserRouter([
         element: <AppLayout />,
         children: [
           { path: "/products", element: <ProductosSupabase/> },
+           { path: "/agregar-items", element: <AgregarItemsPage /> },
           { path: "/modificar-datos", element: <ModificarDatosPage/>},
           { path: "/perfil", element: <PerfilUsuarioPage/>} 
         ]
